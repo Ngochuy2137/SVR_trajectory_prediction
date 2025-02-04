3c563884a00661f1e66a815124a5cfb7e279b45e
Ngochuy2137 <huy.nguyenngoc2137@gmail.com>
2025-02-04 15:18:47 +0900

    Done oop and use our dataset
    Signed-off-by: Ngochuy2137 <huy.nguyenngoc2137@gmail.com>

--- train.py
+++ train.py
@@ -1,121 +1,129 @@
+import os
+import joblib
+import numpy as np
+import pandas as pd
 from sklearn.svm import SVR
 from sklearn.model_selection import GridSearchCV
-from utils.dataset import load_data, load_npz
-from utils.utils import cubic_speed, timestamp
-import joblib
-import pandas as pd
-import numpy as np
-import os
 from multiprocessing import cpu_count
 
+# from utils.dataset import load_npz
+# from utils.utils import cubic_speed
 from python_utils.printer import Printer
 from python_utils.plotter import Plotter
+from nae_static.utils.submodules.training_utils.input_label_generator import InputLabelGenerator
+from nae_static.utils.submodules.training_utils.data_loader import DataLoader as NAEDataLoader
+import wandb
 
-global_util_printer = Printer()
-global_util_plotter = Plotter()
+global_printer = Printer()
+global_plotter = Plotter()
 
-# Xác định lưới tham số (param_grid) để tìm kiếm giá trị tối ưu cho mô hình SVR
-# Các tham số bao gồm 'C' (điều chỉnh mức độ phạt cho lỗi) và 'gamma' (độ ảnh hưởng của các điểm trong kernel RBF)
-# Lựa chọn sử dụng hàm mũ lũy thừa của 2 để tăng độ chính xác trong tìm kiếm tham số
-# param_grid = {'C': np.linspace(0.1,3,30), 'gamma': np.linspace(0.01,0.6,60)}
-param_grid = {
-    'C': np.array([2**(i-5) for i in range(20)]), 
-    'gamma': np.array([2**(i-15) for i in range(20)])
-}
-# param_grid = {'C': [1e-3, 1e-2, 1e-1, 1, 10, 100, 1000, 10000], 'gamma': [10, 1, 0.1, 0.01, 0.001]}
+class SVRTrainer:
+    def __init__(self, object_name, data_dir, spline_type='univariate-k3-s0'):
+        self.object_name = object_name
+        self.data_dir = data_dir
+        self.spline_type = spline_type
+        self.dms_num = 3  # x, y, z
+        self.param_grid = {
+            'C': np.array([2**(i-5) for i in range(20)]), 
+            'gamma': np.array([2**(i-15) for i in range(20)])
+        }
+        self.train_threads = cpu_count() - 4
 
-dms_num = 3 # only pos
-OBJECT_NAME = "trimmed_Bottle_115"
-DATA_DIR = f'/home/server-huynn/workspace/robot_catching_project/trajectory_prediction/trajectory-prediction-SVR/data/{OBJECT_NAME}/training_data'
-# SPLINE_TYPE = 'cubic'
-SPLINE_TYPE = 'univariate-k3-s0'
-'''
-The univariate with k=3, s=0 is indeed a cubic spline.
-'''
+        self.save_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'model', object_name)
+        if not os.path.exists(self.save_path):
+            os.makedirs(self.save_path)
 
+        self.nae_data_loader = NAEDataLoader() 
+        self.input_label_generator = InputLabelGenerator()
+        
+    def load_and_prepare_data(self, folder_dir, data_train_lim=None, data_test_lim=None):
+        inputs_train = []
+        labels_train = []
+        inputs_test = []
+        labels_test = []
+        data_train, _, data_test = self.nae_data_loader.load_train_val_test_dataset(folder_dir, file_format='csv')
+        # limit data
+        if data_train_lim is not None:
+            data_train = data_train[:data_train_lim]
+        if data_test_lim is not None:
+            data_test = data_test[:data_test_lim]
 
-train_threads = cpu_count() - 4 # for stable train
-save_path = 'model'
-if not os.path.exists(save_path):
-    os.makedirs(save_path)
+        # 1.2 generate input and label sequences
+        for d_train in data_train:
+            xya_traj_i  = self.input_label_generator.preprocess_data(d_train, acc_norm=False)
+            for point in xya_traj_i:
+                if point.shape[0] != 9:
+                    raise ValueError(f'point shape is not 9: {point.shape}')
+                inputs_train.append(point[:6])
+                labels_train.append(point[6:])
 
-'''
-Huấn luyện mô hình SVR dựa trên dữ liệu vị trí và lưu mô hình.
-    Args:
-        dms_idx: Chỉ số của thành phần động học (x, y, hoặc z).
-        object_name: Tên đối tượng (ví dụ: 'boomerang').
-'''
-def train_pos(dms_idx):
-    # traj_dir = f"npz_aug_scp/{object_name}/train_q"
-    traj_list = os.listdir(DATA_DIR)
-    model = SVR(kernel='rbf')
-    X = [] # zeta = {[x; v]}
-    y = [] # Y = {a}
-    count = 0
-    for i,traj_npz_name in enumerate(traj_list):
-        # choose the raw data
-        # Chỉ chọn dữ liệu ở mỗi bước thứ 20 để giảm tải
-        if (i+1) % 20 == 0:
-            traj_data = load_npz(DATA_DIR, traj_npz_name)       # shape (time_steps, 3)
-            xva = cubic_speed(traj_data, SPLINE_TYPE)                    # shape ((x, v, a), 3, time_steps)
+        for d_test in data_test:
+            xya_traj_i    = self.input_label_generator.preprocess_data(d_test)
+            for point in xya_traj_i:
+                if point.shape[0] != 9:
+                    raise ValueError(f'point shape is not 9: {point.shape}')
+                inputs_test.append(point[:6])
+                labels_test.append(point[6:])
 
-
-            # # plot ax, ay, az
-            # acc_x = xva[2,0,:]
-            # acc_y = xva[2,1,:]
-            # acc_z = xva[2,2,:]
-            # # x_values = np.arange(len(acc_x))
-            # time = timestamp(acc_x)[1]
-            # vel_interpolation_method = 'cubic'
-            # acc_interpolation_method = 'cubic'
-            # global_util_plotter.plot_line_chart(x_values=time, y_values=[acc_x, acc_y, acc_z], \
-            #                                     title=f'222222 Acceleration - {object_name}  |  VEL_interpolate: {vel_interpolation_method} - ACC_interpolate: {acc_interpolation_method}', \
-            #                                     x_label='Time step', y_label='Acceleration x', \
-            #                                     legends=['acc_x', 'acc_y', 'acc_z'])
-            # input()
+        inputs_train = np.array(inputs_train)
+        labels_train = np.array(labels_train)
+        inputs_test = np.array(inputs_test)
+        labels_test = np.array(labels_test)
+        return (inputs_train, labels_train), (inputs_test, labels_test)
 
 
-            count += xva.shape[2]
-            for i in range(xva.shape[2]):   # shape[2] = time_steps
-                X.append(xva[0:2,:,i].reshape(2*dms_num))   # Get x, v at each time step    -> Input data
-                y.append(xva[2,dms_idx,i])                  # Get a at each time step       -> Label
-    '''
-    Input data - X: được gộp từ x, v từ tất cả quỹ đạo
-    Label - y: được gộp từ a từ tất cả quỹ đạo
-    - Mô hình SVR không phụ thuộc vào chuỗi thời gian, chỉ phụ thuộc vào từng điểm dữ liệu
-    - Dữ liệu từ mỗi điểm dữ liệu (bước thời gian) được coi là một điểm dữ liệu độc lập.
-    '''
-    X = np.array(X)
-    y = np.array(y)
-    global_util_printer.print_green(f'count: {count}')
-    global_util_printer.print_green(f'X.shape: {X.shape}, y.shape: {y.shape}')
-    
-    # Sử dụng GridSearchCV để tìm tham số tối ưu từ lưới tham số param_grid
-    grid_search = GridSearchCV(model, param_grid, n_jobs = train_threads, verbose=1)
-    grid_search.fit(X, y)
-    # Lấy các tham số tối ưu từ kết quả tìm kiếm
-    best_parameters = grid_search.best_estimator_.get_params()
-    for para, val in list(best_parameters.items()):
-        print(para, val)
-    model = SVR(kernel='rbf', C=best_parameters['C'], gamma=best_parameters['gamma'])
-    model.fit(X, y)
+    def train_each_axis(self, axis_idx, x_train, y_train):
+        print('X shape:', x_train.shape)  # (11270, 6)
+        print('y shape:', y_train.shape)  # (11270,)
+        input()
+        '''
+        1. Khởi tạo mô hình SVR và Grid Search để tìm ra các tham số tốt nhất cho mô hình SVR: C và gamma
+        '''
+        global_printer.print_green('    Step 1: Init SVR model and Grid Search')
+        model = SVR(kernel='rbf')
+        grid_search = GridSearchCV(model, self.param_grid, n_jobs=self.train_threads, verbose=1)
+        grid_search.fit(x_train, y_train)
 
-    # Lưu mô hình đã huấn luyện
-    model_path = f"{save_path}/{OBJECT_NAME}_{dms_idx}.pkl"
-    joblib.dump(model, model_path)
-    # Ghi các tham số tối ưu vào file params.txt để tham khảo sau này
-    param_f = open(f"{save_path}/params.txt", "a")
-    param_f.write(f"{OBJECT_NAME}_{dms_idx}: C:{best_parameters['C']} gamma:{best_parameters['gamma']}\n")
-    param_f.close()
-    global_util_printer.print_green(f"Model {OBJECT_NAME}_{dms_idx} was saved to {model_path}")
+        '''
+        2. Huấn luyện lại mô hình SVR với các tham số tối ưu trên toàn bộ tập dữ liệu để đạt hiệu suất tốt nhất.
+        '''
+        global_printer.print_green('    Step 2: Find best params C and gamma')
+        best_params = grid_search.best_estimator_.get_params()
+        for param, val in best_params.items():
+            print(f'{param}: {val}')
+
+        '''
+        3. Huấn luyện mô hình cuối cùng với tham số tối ưu
+        '''
+        global_printer.print_green('    Step 3: Train final model with best params')
+        model = SVR(kernel='rbf', C=best_params['C'], gamma=best_params['gamma'])
+        model.fit(x_train, y_train)
+
+        '''
+        4. Lưu mô hình và các tham số tối ưu
+        '''
+        global_printer.print_green('    Step 4: Save model and best params')
+        model_path = f"{self.save_path}/{self.object_name}_{axis_idx}.pkl"
+        joblib.dump(model, model_path)
+        with open(f"{self.save_path}/params.txt", "a") as param_f:
+            param_f.write(f"{self.object_name}_{axis_idx}: C:{best_params['C']} gamma:{best_params['gamma']}\n")
+        global_printer.print_green(f"       Model {self.object_name} with axis ID {axis_idx} saved to {model_path}")
+
+    def train_axisxyz(self, data_dir, data_train_lim=None, data_test_lim=None):
+        self.data_train, self.data_test = self.load_and_prepare_data(folder_dir=data_dir, data_train_lim=data_train_lim, data_test_lim=data_test_lim)
+        x_train = self.data_train[0]
+        y_train = self.data_train[1]
+        # train x, y, z
+        for axis_idx in range(self.dms_num):
+            global_printer.print_blue(f'----------  Traing AXIS IDX: {axis_idx}  ----------', background=True)
+            self.train_each_axis(axis_idx=axis_idx, x_train=x_train, y_train=y_train[:, axis_idx])
 
 
 if __name__ == '__main__':
-    '''
-    Với mỗi dữ liệu trajectory, huấn luyện mô hình SVR cho để dự đoán cho từng giá trị gia tốc theo trục x, y, z
-    i = 0 -> x
-    i = 1 -> y
-    i = 2 -> z
-    '''
-    for i in range(dms_num):
-        train_pos(i)
+    DATA_DIR = f'/home/server-huynn/workspace/robot_catching_project/trajectory_prediction/mocap_ws/src/mocap_data_collection/data/ball/3-data-augmented/data_plit'
+    OBJECT_NAME = "ball"
+    DATA_TRAIN_LIM = None
+    DATA_TEST_LIM = None
+
+    trainer = SVRTrainer(object_name=OBJECT_NAME, data_dir=DATA_DIR)
+    trainer.train_axisxyz(data_dir=DATA_DIR, data_train_lim=DATA_TRAIN_LIM, data_test_lim=DATA_TEST_LIM)
